ef6cbbbd3c86bdbaba4f0a36ca60a0aa6c11212e
david <david.ndungu@student.moringaschool.com>
2021-04-26 15:17:17 +0300

    this adds function to the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,27 @@ $("document").ready(function () {
       dateFormat: "mm/dd/yy",
     });
   });
+  $(document).ready(function () {
+    $(".popup-btn").click(function () {
+      $(".popup")
+        .css("display", "flex")
+        .css("animation", "popup-bg 1s")
+        .css("animation-interaction-count", "1");
+      $(".popup-wrap")
+        .css("animation", "popup 1s")
+        .css("animation-iteration-count", "1");
+    });
+  
+    $(".close").click(function () {
+      $(".popup-wrap")
+        .css("animation", "popup 1s")
+        .css("animation-iteration-count", "1");
+      $(".popup").delay(500).fadeOut();
+    });
+  });
+  
+  $(document).ready(function () {
+    $(".submit-btn").click(function () {
+      $("#display").toggle("slow");
+    });
+  });
