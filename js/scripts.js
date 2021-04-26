fa4a2f12f1c63d5cd5f58afabf97b8b84e2504f2
david <david.ndungu@student.moringaschool.com>
2021-04-26 15:21:31 +0300

    this adds an array for both males and femalesand a function tothe getting gender

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,30 @@ $("document").ready(function () {
       $("#display").toggle("slow");
     });
   });
+  var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+  
+  function dayOfBirth() {
+    var birthDate = document.getElementById("datepicker").value;
+    var date = new Date(birthDate);
+    var dayOfBirth = date.getDay();
+    //get gender
+    var genders = document.getElementsByName("gender");
+    var gender_value;
+    for (var i = 0; i < genders.length; i++) {
+      if (genders[0].checked) {
+        gender_value = "female";
+      }
+      if (genders[1].checked) {
+        gender_value = "male";
+      }
+    }
